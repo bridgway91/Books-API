b36be88c866130efb4ee43452d2e4351da35c555
Brandon Ridgway <bridgway91@gmail.com>
2023-10-27 22:58:12 -0400

    did a bunch of work but got stuck on API mess, asked eoin for help but think he went to sleep, so can try again tomorrow if/when he responds

--- js/main.js
+++ js/main.js
@@ -3,44 +3,82 @@ document.getElementById('search-data').addEventListener('click', getBook);
 document.querySelector('body').addEventListener('click', toggleBookList);
 document.querySelector('body').addEventListener('click', toggleShowAuthor);
 
-let isbn, title, author, subject, place, person, publisher;
+let isbn, title, author, subject, place, person, publisher, results;
 
 function getBook(){
-  isbn = document.getElementById('isbn').value;
+  isbn = Number(document.getElementById('isbn').value);
   title = document.getElementById('title').value;
   author = document.getElementById('author').value;
   subject = document.getElementById('subject').value;
   place = document.getElementById('place').value;
   person = document.getElementById('person').value;
   publisher = document.getElementById('publisher').value;
+
+  results = document.getElementById('search-results');
+
   if (event.target.id == 'search-isbn') {
-    console.log('isbn search'+' '+isbn);
+    document.getElementById('isbn-error').style.display = 'none';
+    if (isNaN(isbn) || (`${isbn}`.length != 10 && `${isbn}`.length != 13)) {
+      document.getElementById('isbn-error').style.display = 'inline';
+    } else { // 9780140328721  -- fantastic mr fox, for testing
+      fetch(`https://openlibrary.org/isbn/${isbn}.json`)
+        .then(res => res.json())
+        .then(data => {
+          results.innerHTML = '';
+          console.log(data);
+
+          let resCover = `https://covers.openlibrary.org/b/isbn/${isbn}-S.jpg`;
+          let resTitle = data.title;
+          let resAuthor;
+          fetch(`https://openlibrary.org${data.authors[0].key}.json`)
+            .then(res => res.json())
+            .then(data => {
+              resAuthor = data.name;
+            })
+            .catch(err => {
+              console.log(`error ${err}`)
+            });
+
+          let resPublisher = data.publishers[0];
+          let resPubDate = data.publish_date;
+
+          results.innerHTML = 
+            `<div id="result-0">
+              <button>+</button>
+              <img src="${resCover}" alt="cover">
+              <span>${resTitle}</span>
+              <span>${resAuthor}</span>
+              <span>${resPublisher}</span>
+              <span>${resPubDate}</span>
+            </div>`;
+        })
+        .catch(err => {
+          console.log(`error ${err}`)
+        });
+    }
   } else if (event.target.id == 'search-data') {
     console.log('data search');
   };
-
-  // fetch(url)
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       console.log(data);
-  //     })
-  //     .catch(err => {
-  //       console.log(`error ${err}`)
-  //     });
 };
 
-// function seturl() {
-//   if (event.target.id == 'search-isbn') {
-//     console.log('isbn search'+' '+isbn);
-//   } else if (event.target.id == 'search-data') {
-//     console.log('data search');
-//   };
+// function fetchAuthor(key) {
+//   let authorName = '';
+//   fetch(`https://openlibrary.org${key}.json`)
+//     .then(res => res.json())
+//     .then(data => {
+//       authorName = data.name;
+//       console.log(authorName);
+//       return authorName;
+//     })
+//     .catch(err => {
+//       console.log(`error ${err}`)
+//     });
+//   // return authorName;
 // };
 
 function toggleBookList() {
   if (event.target.tagName != 'BUTTON') return;
   if (event.target.id == 'search-isbn' || event.target.id == 'search-data') return;
-  // console.log(event.target.parentNode.id);
   if (event.target.innerText == '+') {
     event.target.innerText = '-';
   } else if (event.target.innerText == '-') {
