2fe3479261a3ecc175cec388c139004e7901b6c6
Brandon Ridgway <bridgway91@gmail.com>
2023-11-10 17:11:18 -0500

    added light/dark mode toggle, yay

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 updateMyList();
-
+setLighting();
 
 document.getElementById('search-isbn').addEventListener('click', getBook);
 document.getElementById('search-data').addEventListener('click', getBook);
@@ -11,6 +11,7 @@ document.getElementById('subject').addEventListener('input', checkInputs);
 document.getElementById('place').addEventListener('input', checkInputs);
 document.getElementById('person').addEventListener('input', checkInputs);
 document.getElementById('publisher').addEventListener('input', checkInputs);
+document.getElementById('lightToggle').addEventListener('click', lightToggle);
 
 let isbn, title, author, subject, place, person, publisher, results;
 let spanLength = 30;
@@ -205,14 +206,14 @@ function updateMyList() {
     if (listAuthors.includes(collection[i][0])) {
       let listSingleAuthor = document.getElementById(`${collection[i][0].replace(' ','')}`);
       let authorBooks = listSingleAuthor.nextElementSibling;
-      authorBooks.innerHTML += `<li data-isbn=${collection[i][4]}>${collection[i][1]} - ${collection[i][3]}, ${collection[i][2]}</li>`;
+      authorBooks.innerHTML += `<li data-isbn=${collection[i][4]}><span>${collection[i][1]}</span> -- <strong>${collection[i][3]}  :</strong>  ${collection[i][2]}</li>`;
     } else {
       listAuthors.push(collection[i][0]);
       myList.innerHTML += 
         `<div>
           <h3 id=${collection[i][0].replace(' ','')}>${collection[i][0]}</h3>
           <ul data-show="true">
-            <li data-isbn=${collection[i][4]}>${collection[i][1]} - ${collection[i][3]}, ${collection[i][2]}</li>
+            <li data-isbn=${collection[i][4]}><span>${collection[i][1]}</span> -- <strong>${collection[i][3]}  :</strong>  ${collection[i][2]}</li>
           </ul>
         </div>`;
     };
@@ -256,4 +257,28 @@ function checkList(isbn) {
   } else {
     return '<i class="fa-regular fa-square-plus"></i>';
   }
+}
+
+function lightToggle() {
+  console.log('let there be light (or dark)')
+  let lighting = localStorage.getItem('lighting');
+  if (lighting === 'dark') {
+    localStorage.setItem('lighting', 'light');
+  } else {
+    localStorage.setItem('lighting', 'dark');
+  };
+  setLighting();
+}
+
+function setLighting() {
+  let lighting = localStorage.getItem('lighting');
+  if (lighting === 'dark') {
+    document.body.classList.add('dark');
+    document.getElementById('lightToggle').innerHTML = 
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17.75,4.09L15.22,6.03L16.13,9.09L13.5,7.28L10.87,9.09L11.78,6.03L9.25,4.09L12.44,4L13.5,1L14.56,4L17.75,4.09M21.25,11L19.61,12.25L20.2,14.23L18.5,13.06L16.8,14.23L17.39,12.25L15.75,11L17.81,10.95L18.5,9L19.19,10.95L21.25,11M18.97,15.95C19.8,15.87 20.69,17.05 20.16,17.8C19.84,18.25 19.5,18.67 19.08,19.07C15.17,23 8.84,23 4.94,19.07C1.03,15.17 1.03,8.83 4.94,4.93C5.34,4.53 5.76,4.17 6.21,3.85C6.96,3.32 8.14,4.21 8.06,5.04C7.79,7.9 8.75,10.87 10.95,13.06C13.14,15.26 16.1,16.22 18.97,15.95M17.33,17.97C14.5,17.81 11.7,16.64 9.53,14.5C7.36,12.31 6.2,9.5 6.04,6.68C3.23,9.82 3.34,14.64 6.35,17.66C9.37,20.67 14.19,20.78 17.33,17.97Z" /></svg>';
+  } else {
+    document.body.classList.remove('dark');
+    document.getElementById('lightToggle').innerHTML = 
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3.55 19.09L4.96 20.5L6.76 18.71L5.34 17.29M12 6C8.69 6 6 8.69 6 12S8.69 18 12 18 18 15.31 18 12C18 8.68 15.31 6 12 6M20 13H23V11H20M17.24 18.71L19.04 20.5L20.45 19.09L18.66 17.29M20.45 5L19.04 3.6L17.24 5.39L18.66 6.81M13 1H11V4H13M6.76 5.39L4.96 3.6L3.55 5L5.34 6.81L6.76 5.39M1 13H4V11H1M13 20H11V23H13" /></svg>'
+  };
 }
